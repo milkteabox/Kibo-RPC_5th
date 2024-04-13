22a3155e96ac3905adf2b5b601d34eebdfd3a4ff
spig100 <spig100.roc@gmail.com>
2024-04-13 13:58:32 +0800

    add PathRunner class and finish the init features

---
+++ app/src/main/java/jp/jaxa/iss/kibo/rpc/defaultapk/PathRunner.java
@@ -0,0 +1,11 @@
+package jp.jaxa.iss.kibo.rpc.defaultapk;
+
+import java.util.Vector;
+
+import gov.nasa.arc.astrobee.types.Point;
+
+public class PathOptimizer {
+    public static void AStar(Point startPoint, Poin endPoint){
+        
+    }
+}
